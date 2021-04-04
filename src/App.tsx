bec41a9cb99fde6bfc407a2f2b59557dc5321e14
Charles Pereira <charlespereira.fln@gmail.com>
2021-04-04 01:38:19 -0300

    Aula 03-04

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { FormEvent, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const onSubmit = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -12,6 +17,17 @@ function App() {
           </br>Aplicação desenvolvida em TypeScript + Docker <br>
           </br>Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <form onSubmit={onSubmit}>
+          <div>
+            <label htmlFor="name">Nome: </label>
+            <input type="text" id="name" ref={nameRef}/>
+          </div>
+          <div>
+            <label htmlFor="email">Email: </label>
+            <input type="email" id="email"/>
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
         <a
           className="App-link"
           href="https://reactjs.org"
